33ae506be75bfe0f6b4804c43739662525c9a0b5
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-11-30 21:46:28 -0300

    upload beecrowd

---
+++ source/beecrowd/1005/1005/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd1005
+{
+    internal class Program {
+        static void Main(string[] args) {
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double media = ((a * 3.5) + (b * 7.5)) / 11;
+            Console.WriteLine($"MEDIA = {media.ToString("F5", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ source/beecrowd/1006/1006/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd1006
+{
+    internal class Program {
+        static void Main(string[] args) {
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = ((a * 2) + (b * 3) + (c * 5)) / 10;
+            Console.WriteLine($"MEDIA = {media.ToString("F1", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
