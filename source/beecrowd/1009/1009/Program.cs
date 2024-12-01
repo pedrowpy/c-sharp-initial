e28e79ca790340935e00825554be48f1d7af54fa
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-11-30 21:47:22 -0300

    upload beecrowd

---
+++ source/beecrowd/1009/1009/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd1009
+{
+    internal class Program {
+        static void Main(string[] args) {
+            string nome = Console.ReadLine();
+            double salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double totalVenda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salarioTotal = (double)(totalVenda * 15.0) / 100 + salarioFixo;
+
+            Console.WriteLine($"TOTAL = R$ {salarioTotal.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ source/beecrowd/1011/1011/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd1011
+{
+    internal class Program {
+        static void Main(string[] args) {
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double volume = (double) (4 / 3.0) * 3.14159 * Math.Pow(raio, 3.0);
+            Console.WriteLine($"VOLUME = {volume.ToString("F3", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
