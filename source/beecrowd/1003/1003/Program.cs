73f8cf33bf0e98e9926f2f86e44ad74672da4dfe
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-11-30 21:45:23 -0300

    upload beecrowd

---
+++ source/beecrowd/1003/1003/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace beecrowd1003
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite o primeiro valor de A");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digie o segundo valor B");
+            int B = int.Parse(Console.ReadLine());
+
+            int SOMA = A + B;
+
+            Console.WriteLine($"SOMA = {SOMA}");
+        }
+    }
+}
---
+++ source/beecrowd/1004/1004/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Threading.Channels;
+
+namespace beecrowd1004 
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int prod = a * b;
+            Console.WriteLine($"PROD = {prod}");
+        }
+    }
+}
