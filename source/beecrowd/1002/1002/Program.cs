4e5fdd58d55f0347cb2cad7c5190592861a8f117
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-11-30 21:44:29 -0300

    upload beecrowd

---
+++ source/beecrowd/1002/1002/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd1002
+{
+    internal class Program {
+        static void Main(string[] args) {
+            double raio, area, pi = 3.14159;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = pi * raio * raio;
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
