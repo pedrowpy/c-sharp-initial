b16000c5bee40b5d0b1e9b305d25d2885d2f3e32
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-01 13:02:43 -0300

    exercises beecrowd

---
+++ source/beecrowd/1012/1012/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd1012
+{
+    internal class Program {
+        static void Main(string[] args) {
+            string[] valores = Console.ReadLine().Split(' ');
+            double a = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            double b = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            double triangulo = (double)a * c / 2.0;
+            double circulo = (double)3.14159 * Math.Pow(c, 2.0);
+            double trapezio = (double)(a + b) / 2.0 * c;
+            double quadrado = (double)b * b;
+            double retangulo = (double)b * a;
+
+            Console.WriteLine($"TRIANGULO: {triangulo.ToString("F3", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"CIRCULO: {circulo.ToString("F3", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"TRAPEZIO: {trapezio.ToString("F3", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"QUADRADO: {quadrado.ToString("F3", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"RETANGULO: {retangulo.ToString("F3", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ source/beecrowd/1014/1014/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd1014 {
+    internal class Program {
+        static void Main(string[] args) {
+            int a = int.Parse(Console.ReadLine());
+            double km = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double kml = (double)a / km;
+
+            Console.WriteLine($"{kml.ToString("F3", CultureInfo.InvariantCulture)} km/l");
+        }
+    }
+}
