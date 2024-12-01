e5523cdaf8d9518a48c7a7f2f4b23bbd28caf1d3
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-11-30 21:43:00 -0300

    upload exercises

---
+++ source/c#-exercises/exercise-2/exercise-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercise2
+{
+    internal class Program {
+    static void Main(string[] args) {
+        double b, a, area, perimetro, diagonal;
+        b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        area = b * a;
+        perimetro = 2 * (b + a);
+        diagonal = Math.Sqrt(Math.Pow(b, 2.0) + Math.Pow(a, 2.0));
+
+        Console.WriteLine($"AREA = {area.ToString("F4", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"PERIMETRO = {perimetro.ToString("F4", CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"diagonal = {diagonal.ToString("F4", CultureInfo.InvariantCulture)}");
+        Console.ReadLine();
+    }
+}
+}
---
+++ source/c#-exercises/exercise-3/exercise-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercise3
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite o primeiro nome:");
+            string nome1 = Console.ReadLine();
+            Console.WriteLine($"Digite a idade de {nome1}");
+            int idade1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo nome:");
+            string nome2 = Console.ReadLine();
+            Console.WriteLine($"Digite a idade de {nome2}");
+            int idade2 = int.Parse(Console.ReadLine());
+
+            double media = (double) (idade1 + idade2) / 2.0;
+
+            Console.WriteLine($"A idade média de {nome1} e {nome2} é de {media.ToString("F1", CultureInfo.InvariantCulture)} anos");
+        }
+    }
+}
