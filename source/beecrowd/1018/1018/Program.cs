b6c3fda39b476e8a9b6061b8e76ea27d8a826ed4
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-01 13:03:46 -0300

    exercises beecrowd

---
+++ source/beecrowd/1018/1018/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Xml;
+
+namespace beecrowd1019 {
+    internal class Program {
+        static void Main(string[] args) {
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine(N);
+
+            int resto = N;
+
+            int nota = 100;
+            int quociente = resto / nota;
+            Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+            resto = resto % nota;
+
+            nota = 50;
+            quociente = resto / nota;
+            Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+            resto = resto % nota;
+
+            nota = 20;
+            quociente = resto / nota;
+            Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+            resto = resto % nota;
+
+            nota = 10;
+            quociente = resto / nota;
+            Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+            resto = resto % nota;
+
+            nota = 5;
+            quociente = resto / nota;
+            Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+            resto = resto % nota;
+
+            nota = 2;
+            quociente = resto / nota;
+            Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+            resto = resto % nota;
+
+            Console.WriteLine($"{resto} nota(s) de R$ 1,00");
+        }
+    }
+}
---
+++ source/beecrowd/1020/1020/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace beecrowd1020 {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int anos = n / 365;
+            int resto = n % 365;
+            int meses = resto / 30;
+            int dias = resto % 30;
+
+            Console.WriteLine($"{anos} ano(s)");
+            Console.WriteLine($"{meses} mes(es)");
+            Console.WriteLine($"{dias} dia(s)");
+        }
+    }
+}
