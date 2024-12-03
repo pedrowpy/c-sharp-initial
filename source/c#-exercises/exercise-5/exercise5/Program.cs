cf4cbf2f6b4772234c92f9f431818f5fc11b247d
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-02 21:03:38 -0300

    exercise5

---
+++ source/c#-exercises/exercise-5/exercise5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace exercise5 {
+    internal class Program {
+        static void Main(string[] args) {
+            string[] vet = (Console.ReadLine().Split(' '));
+            int a = int.Parse(vet[0]);
+            int b = int.Parse(vet[1]);
+            int c = int.Parse(vet[2]);
+
+            if (a < b && a < c) {
+                Console.WriteLine($"MENOR = {a}");
+            } if (b < a && b < c) {
+                Console.WriteLine($"MENOR = {b}");
+            } else {
+                Console.WriteLine($"MENOR = {c}");
+            }
+        }
+
+    }
+}
+
