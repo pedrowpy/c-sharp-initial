e3d3d99bd61958327184806499aa752a724f0f4a
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-03 20:54:15 -0300

    exercises beecrowd

---
+++ source/beecrowd/1035/1035/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace beecrowd1035
+{
+    internal class Program {
+        static void Main(string[] args) {
+            string[] vet = Console.ReadLine().Split(' ');
+            int a = int.Parse(vet[0]);
+            int b = int.Parse(vet[1]);
+            int c = int.Parse(vet[2]);
+            int d = int.Parse(vet[3]);
+
+            if (b > c && d > a && (c + d) > (b + a) && (c > 0 && d > 0) && (a % 2 == 0)) {
+                Console.WriteLine("Valores aceitos");
+            } else {
+                Console.WriteLine("Valores nao aceitos");
+            }
+        }
+    }
+}
---
+++ source/beecrowd/1048/1048/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace beecrowd1044 {
+    internal class Program {
+        static void Main(string[] args) {
+            double salarioInicio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double percentual = 0;
+
+            if (salarioInicio >= 0 && salarioInicio <= 400.00) {
+                percentual = 15.0;
+            } else if (salarioInicio >= 400.01 && salarioInicio <= 800.00) {
+                percentual = 12.0;
+            } else if (salarioInicio >= 800.01 && salarioInicio <= 1200.00) {
+                percentual = 10.0;
+            } else if (salarioInicio >= 1200.01 && salarioInicio <= 2000.00) {
+                percentual = 7.0;
+            } else if (salarioInicio > 2000.00) {
+                percentual = 4.0;
+            }
+
+            double reajuste = salarioInicio * percentual / 100;
+            double novoSalario = salarioInicio + reajuste;
+
+            Console.WriteLine($"Novo salario: {novoSalario.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Reajuste ganho: {reajuste.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Em percentual: {percentual} %");
+        }
+    }
+}
