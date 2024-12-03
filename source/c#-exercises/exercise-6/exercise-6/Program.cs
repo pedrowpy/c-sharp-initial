4c64c807b34c9f874910c939c69e75d8a5e2c347
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-02 21:14:53 -0300

    exercise6

---
+++ source/c#-exercises/exercise-6/exercise-6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercise6 {
+    internal class Program {
+        static void Main(string[] args) {
+            int minutos;
+            double conta;
+
+            minutos = int.Parse(Console.ReadLine());
+
+            conta = 50.0;
+            if (minutos > 100) {
+                conta += (minutos - 100) * 2.0;
+            }
+
+            Console.WriteLine($"Valor a pagar: R$ {conta.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.ReadLine();
+        }
+    }
+}
