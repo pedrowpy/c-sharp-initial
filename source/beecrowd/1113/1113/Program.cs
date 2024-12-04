ea970e2985c961fc567c310f96d565786be7f853
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-03 21:12:13 -0300

    exercise beecrowd

---
+++ source/beecrowd/1113/1113/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace beecrowd1113 {
+    internal class Program {
+        static void Main(string[] args) {
+            int x, y;
+            string[] vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            while (x != y) {
+                if (x < y) {
+                    Console.WriteLine("Crescente");
+                } else {
+                    Console.WriteLine("Decrescente");
+                }
+                vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+            }
+        }
+    }
+}
