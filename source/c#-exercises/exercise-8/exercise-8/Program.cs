e9261dbe41db1ffd036fa7096557aed1c2dd602a
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-03 21:19:13 -0300

    exercise8 up

---
+++ source/c#-exercises/exercise-8/exercise-8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercise8 {
+    internal class Program {
+        static void Main(string[] args) {
+            double idade, soma, media;
+            int count;
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            soma = 0.0;
+            count = 0;
+            while (idade > 0) {
+                soma = soma + idade;
+                count = count + 1;
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            if (count == 0) {
+                Console.WriteLine("Impossivel calcular");
+            } else { 
+            media = soma / count;
+                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
