4715e52a36e384164b3d1e039475d8aa315f717f
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-04 22:25:58 -0300

    beecrowd upload

---
+++ source/beecrowd/1078/1078/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace beecrowd1078
+{
+    internal class Program {
+        static void Main(string[] args) {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i=1; i<=10; i++) {
+                int resultado = i * N;
+                Console.WriteLine($"{i} x {N} = {resultado}");
+            }
+        }
+    }
+}
