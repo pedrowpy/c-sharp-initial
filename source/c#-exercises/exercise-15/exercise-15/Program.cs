3668049de3e45bb89eafef0aa26a729196a3db07
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-04 22:28:32 -0300

    exercises upload

---
+++ source/c#-exercises/exercise-15/exercise-15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercise15 {
+    internal class Program {
+        static void Main(string[] args) {
+            int N;
+            N = int.Parse(Console.ReadLine());
+            string[] vet = new string[N];
+
+            for (int i = 0; i < N; i++) {
+                vet[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Nomes lidos: ");
+            foreach (string elemento in vet) {
+                Console.WriteLine(elemento);
+            }
+        }
+    }
+}  
