e0a45c1726342f900b93d70dcb680b6ce2b637fc
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-04 22:27:45 -0300

    exercises upload

---
+++ source/c#-exercises/exercise-12/exercise-12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace exercise12 {
+    internal class Program {
+        static void Main(string[] args) {
+            int N;
+            int[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new int[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++) {
+                vet[i] = int.Parse(s[i]);
+            }
+
+            for (int i = 0; i < N; i++) {
+                if (vet[i] < 0) {
+                    Console.WriteLine(vet[i]);
+                }
+            }
+        }
+    }
+}
