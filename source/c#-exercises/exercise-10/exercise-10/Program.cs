a8ec0ce8369ffdd36137e27e7e8f42ff8ed51a95
Pedro Fonseca <132118855+pedrowpy@users.noreply.github.com>
2024-12-04 22:26:45 -0300

    exercises upload

---
+++ source/c#-exercises/exercise-10/exercise-10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercise10 {
+    internal class Program {
+        static void Main(string[] args) {
+            double c, f;
+            char repetir;
+
+            do {
+                Console.WriteLine("Digite a temperatura em Celsius: ");
+                c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                f = (double)9.0 * c / 5.0 + 32;
+                Console.WriteLine($"Equivalente em Fahrenheit: {f.ToString("F1", CultureInfo.InvariantCulture)}");
+                Console.Write("Deseja repetir (s/n)?");
+                repetir = char.Parse(Console.ReadLine());
+            } while (repetir == 's');
+        }
+    }
+}
